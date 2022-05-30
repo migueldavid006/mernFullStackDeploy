a10f0aa78893995e45837881a408c0ef8a7bd51a
migueldavid006 <migueldavid006@hotmail.com>
2022-05-29 22:14:41 -0500

    fullstack deploy total

---
+++ portafolio_en_mejora/main.js
@@ -0,0 +1,139 @@
+const btnDepartamentos = document.getElementById('btn-departamentos'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('grid'),
+	  contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlaces-nav'),
+	  contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
+	  esDispositivoMovil = () => window.innerWidth <= 800;
+
+btnDepartamentos.addEventListener('mouseover', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.add('activo');
+	}
+});
+
+grid.addEventListener('mouseleave', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.remove('activo');
+	}
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		};
+	});
+});
+
+// EventListeners para dispositivo movil.
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow = 'visible';
+	} else {
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+// Click en boton de todos los departamentos (Para version movil).
+btnDepartamentos.addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		}
+	});
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+	boton.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e)=> {
+	e.preventDefault();
+	document.querySelectorAll('#menu .activo').forEach((elemento) => {
+		elemento.classList.remove('activo');
+	});
+	document.querySelector('body').style.overflow = 'visible';
+});
+
+
+const grid2 =  new Muuri('.grid2',{
+	layout:{
+		rounding: false
+	}
+});
+
+window.addEventListener('load',()=>{
+	grid2.refreshItems().layout();
+	document.getElementById('grid2').classList.add('imagenes-cargadas');
+
+	
+	const enlaces2 = document.querySelectorAll('#categorias2 a');
+	enlaces2.forEach( (elemento)=>{
+		elemento.addEventListener('click',(evento)=>{
+			evento.preventDefault();
+			enlaces2.forEach((enlace)=>enlace.classList.remove('activo'))
+			evento.target.classList.add('activo');
+
+			const categoriatipo = evento.target.innerHTML.toLowerCase();
+			categoriatipo === 'todo' ? grid2.filter('[data-categoria]') : grid2.filter(`[data-categoria="${categoriatipo}"]`);
+		});
+	});
+
+// agregando listener para la barra de busqueda
+
+const overlay = document.getElementById('overlay');
+document.querySelectorAll('.grid2 .item img').forEach((elemento)=>{	
+		elemento.addEventListener('click',() =>{
+			const ruta = elemento.getAttribute('src')
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+
+	});
+
+
+// addevent listener del boton cerrar
+document.querySelector('#btn-cerrar-popup').addEventListener('click',() =>{
+	overlay.classList.remove('activo');
+});
+
+//  eventi listener de overlay
+overlay.addEventListener('click',(evento)=>{
+ evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+});
+});
+
